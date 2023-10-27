0b13a825e1b846c0adc28776bc007b9ef3aec068
KumarDevada <115659253+KumarDevada@users.noreply.github.com>
2023-10-27 10:14:03 +0530

    ..

--- CampusTradeClient/src/main.jsx
+++ CampusTradeClient/src/main.jsx
@@ -2,14 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ThemeProvider } from "@mui/material"
 import App from './App.jsx'
-import FDFED from './FDFED.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   
     
       <App />
-      // <FDFED />
     
  
 )
