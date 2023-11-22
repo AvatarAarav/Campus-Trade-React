cf576c93e7c64cebf20ef6fda8b27064ba8c2027
PratSins <pratyush2002ps@gmail.com>
2023-11-22 11:19:07 +0530

    AboutUS

--- CampusTradeClient/src/Pages/AboutUs/AboutUs.jsx
+++ CampusTradeClient/src/Pages/AboutUs/AboutUs.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import './aboutThemes.css'
+import Card from "./Card";
+import contacts from "./contacts";
+
+
+function createCard(contact) {
+  return (
+    <Card
+      key={contact.id}
+      name={contact.name}
+      img={contact.imgURL}
+      prof={contact.linkedin}
+      insta={contact.insta}
+      info={contact.info}
+    />
+  );
+}
+
 
 function AboutUs() {
   return (
-    <div>AboutUs</div>
+    <div>
+      <h1 className="heading">My Contacts</h1>
+      <div className='c2'>
+            {contacts.map(createCard)}
+      </div>
+      <br/>
+    </div>
   )
 }
 
---
+++ CampusTradeClient/src/Pages/AboutUs/contacts.js
@@ -0,0 +1,46 @@
+const contacts = [
+    {
+      name: "Aarav Nigam",
+      imgURL:
+        "/src/Pages/AboutUs//img/aarav.jpg",
+      linkedin: "https://www.linkedin.com/in/aarav-nigam-09982b222/",
+      insta:"https://www.instagram.com/avatar_aarav/",
+      info: "TEAM LEAD"
+    },
+    {
+      name: "Pratyush Singh",
+      imgURL:
+        "/src/Pages/AboutUs//img/prat.png",
+      linkedin: "https://www.linkedin.com/in/pratyush-singh-a09aa3228/",
+      insta:"https://www.instagram.com/avatar_aarav/",
+      info: "Frontend DEV"
+    },
+    {
+      name: "Parth Bhandhari",
+      imgURL:
+        "/src/Pages/AboutUs//img/parth.jpg",
+      linkedin: "https://www.linkedin.com/",
+      insta:"https://www.instagram.com/",
+      info: "Backend DEV"
+    },
+    {
+      name: "Rathod Harshan",
+      imgURL:
+        "/src/Pages/AboutUs//img/harshan.jpg",
+      linkedin: "https://www.linkedin.com/in/rathod-harshan-a0922622b/",
+      insta:"https://www.instagram.com/avatar_aarav/",
+      info: "Frontend DEV"
+    },
+    {
+      name: "Kumar Devda",
+      imgURL:
+        "/src/Pages/AboutUs//img/kumar.jpg",
+      linkedin: "https://www.linkedin.com/in/kumara-swamy-devada-b695a8252/",
+      insta:"https://www.instagram.com/avatar_aarav/",
+      info: "Frontend DEV"
+    }
+
+  ];
+  
+  export default contacts;
+  
