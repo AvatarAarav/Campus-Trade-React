05d94cc2ef8a9a42bcbd79628860dd323e15f336
Rathod <rathodharshan7@gmail.com>
2023-11-26 16:58:43 +0530

    User profile edited

--- CampusTradeClient/src/Pages/UserProfile/ProfileX.jsx
+++ CampusTradeClient/src/Pages/UserProfile/ProfileX.jsx
@@ -1,63 +1,141 @@
 import  { useState } from "react";
-import { Button, Card, CardContent, CardMedia, Typography } from "@mui/material";
+import { ThemeProvider } from '@mui/material/styles';
+import {Box, Button, Card, CardContent, CardMedia, Typography } from "@mui/material";
+import theme from '../../theme.jsx';
 
 function UserData() {
-  const userDataStyle = {
-    textAlign: "center",
-    margin: "20px",
-  };
-
-  const imgStyle = {
-    maxWidth: "100px",
-    borderRadius: "50%",
-  };
-
-  const buttonStyle = {
-    margin: "5px",
-  };
-
   return (
-    <div style={userDataStyle}>
-      <img alt="Profile" style={imgStyle} />
-      <h2 style={{ fontSize: "24px" }}>harshan</h2>
-      <p>Email: rathodharshan4@gmail.com</p>
-      <p>College: IIIT</p>
-      <p>Year: 2023</p>
-      <p>Branch: CSE</p>
-      <button onClick={() => {}} style={buttonStyle}>
-        Edit Profile
-      </button>
-      <button style={buttonStyle}>Post Ad</button>
-    </div>
+    <ThemeProvider theme={theme}>
+    <Box
+      sx={{
+        textAlign: "",
+        margin: "50px",
+        display: "flex",
+        maxWidth: "70%",
+        border: "1px solid",
+        padding: "10px",
+        borderRadius:"20px",
+        backgroundColor: theme.palette.secondary.main,
+      }}
+    >
+      <Box
+        className="redbox"
+        sx={{ padding: "20px", color: "secondary" }}
+      >
+        <img
+          src="https://media.licdn.com/dms/image/D5603AQGNWbFM14-pvg/profile-displayphoto-shrink_800_800/0/1691730643282?e=2147483647&v=beta&t=ZA4LcfBqZT6HW4QKslEZv4tXz7BVXamW7YRObMbSQ64"
+          alt="Profile"
+          style={{ maxWidth: "200px", borderRadius: "100%" }}
+        />
+      </Box>
+      <Box>
+        <Typography variant="h2" sx={{ fontSize: "50px", color: "#fff" }}>
+          Harshan
+        </Typography>
+        <Typography variant="body1" sx={{ color: "#fff" }}>
+          Email: rathodharshan4@gmail.com
+        </Typography>
+        <Typography variant="body1" sx={{ color: "#fff" }}>
+          College: IIIT Sricity
+        </Typography>
+        <Typography variant="body1" sx={{ color: "#fff" }}>
+          Year: 2023
+        </Typography>
+        <Typography variant="body1" sx={{ color: "#fff" }}>
+          Branch: CSE
+        </Typography>
+      </Box>
+
+      <Box
+        className="btns"
+        sx={{
+          display: "flex",
+          justifyContent: "flex-end",
+          minWidth: "350px",
+          marginTop: "200px",
+        }}
+      >
+        <Button
+          onClick={() => {}}
+          sx={{
+            margin: "5px",
+            height: "50px",
+            transition: "background-color 0.3s ease",
+            backgroundColor: theme.palette.primary.main,
+            color: "#fff",
+            border: "none",
+            borderRadius: "5px",
+            '&:hover': {
+              backgroundColor: theme.palette.primary.dark,
+            },
+          }}
+        >
+          Edit Profile
+        </Button>
+        <Button
+          sx={{
+            margin: "5px",
+            height: "50px",
+            transition: "background-color 0.3s ease",
+            backgroundColor: theme.palette.primary.main,
+            color: "#fff",
+            border: "none",
+            borderRadius: "5px",
+            '&:hover': {
+              backgroundColor: theme.palette.primary.dark,
+            },
+          }}
+        >
+          +Post Ad
+        </Button>
+      </Box>
+    </Box>
+    </ThemeProvider>
   );
 }
 
 
-function UserEarnings() {
-  const earningsStyle = {
-    margin: "20px",
-  };
 
+function UserEarnings() {
   return (
-    <div style={earningsStyle}>
-      <p>Ads Posted:7</p>
-      <p>Ads Bought:8</p>
-      <p>Earnings: 500</p>
-      <p>Ads Bought Money: 1000</p>
-      <p>Profit: -500</p>
-    </div>
+    <Box
+      sx={{
+        margin: "50px",
+        marginTop: "70px",
+        border: "1px solid",
+        padding: "20px",
+        borderRadius: "20px",
+        backgroundColor: theme.palette.secondary.main,
+      }}
+    >
+      <Typography variant="body1" sx={{ color: "#fff" }}>
+        Ads Posted: 7
+      </Typography>
+      <Typography variant="body1" sx={{ color: "#fff" }}>
+        Ads Bought: 8
+      </Typography>
+      <Typography variant="body1" sx={{ color: "#fff" }}>
+        Earnings: 500
+      </Typography>
+      <Typography variant="body1" sx={{ color: "#fff" }}>
+        Ads Bought Money: 1000
+      </Typography>
+      <Typography variant="body1" sx={{ color: "#fff" }}>
+        Profit: -500
+      </Typography>
+    </Box>
   );
 }
 
 function AdCard() {
   return (
-    <Card style={{ maxWidth: 200, margin: 10 }}>
+    <Card sx={{ maxWidth: "200px", margin: "10px" }}>
       <CardMedia component="img" height="140"  alt="Ad" />
       <CardContent>
-        <Typography gutterBottom variant="h6" component="div">
+        <Typography gutterBottom variant="h6" component="Box">
           Cost:900
         </Typography>
-        <Button variant="contained" color="primary" style={{ marginRight: 5 }}>
+        <Button variant="contained" color="primary" sx={{ marginRight: "5px" }}>
           Remove
         </Button>
         <Button variant="contained" color="secondary">
@@ -72,12 +150,7 @@ function AdCard() {
 
 function UserProfile() {
   const user = {
-    profileIcon: "profile.jpg",
-    name: "John Doe",
-    email: "john@example.com",
-    college: "Sample College",
-    year: "2023",
-    branch: "Computer Science",
+   
   };
 
   const [adsPosted, setAdsPosted] = useState(2);
@@ -103,21 +176,21 @@ function UserProfile() {
 
   function AdsPosted() {
     return (
-      <div>
+      <Box sx={{display:"flex"}}>
         {postedAds.map((ad, index) => (
           <AdCard key={index} ad={ad} />
         ))}
-      </div>
+      </Box>
     );
   }
   
   function AdsBought() {
     return (
-      <div>
+      <Box sx={{display:"flex"}} >
         {boughtAds.map((ad, index) => (
           <AdCard key={index} ad={ad} />
         ))}
-      </div>
+      </Box>
     );
   }
 
@@ -138,20 +211,22 @@ function UserProfile() {
   };
 
   return (
-    <div>
-      <UserData user={user} onEditProfile={handleEditProfile} />
+    <Box sx={{ backgroundColor: theme.palette.background.default, padding: '30px' }}>
+    <Box sx={{ display: "flex" }}>
+      <UserData user={user} onEditProfile={() => {}} />
       <UserEarnings {...earnings} />
-      <h3>Ads Posted</h3>
-      <AdsPosted postedAds={postedAds} />
-      <h3>Ads Bought</h3>
-      <AdsBought boughtAds={boughtAds} />
-      <Button onClick={handlePostAd} variant="contained" color="primary">
-        Post Ad
-      </Button>
-      <Button onClick={handleBuyAd} variant="contained" color="secondary">
-        Buy Ad
-      </Button>
-    </div>
+    </Box>
+
+    <Typography variant="h3"sx={{ textAlign: "center", border: "1px solid", margin: '20px' ,backgroundColor: theme.palette.primary.default}}>
+      Ads Posted
+    </Typography>
+    <AdsPosted postedAds={postedAds} />
+
+    <Typography variant="h3" sx={{ textAlign: "center", border: "1px solid", margin: '20px',backgroundColor: theme.palette.primary.default }}>
+      Ads Bought
+    </Typography>
+    <AdsBought boughtAds={boughtAds} />
+  </Box>
   );
 }
 
