d6e8f9a8db1b613d04a5aa49d97490c9c89de6c1
bhparth <parthbhandari2663@gmail.com>
2023-12-06 13:07:01 +0530

    c

--- server/controllers/unbuy_product.js
+++ server/controllers/unbuy_product.js
@@ -3,12 +3,18 @@ import Products from "../db/Models/Products";
 
 export const UnbuyAPI=async (req,res)=>{
     try {
-       const pid = req.body.id;
-       const uid = req.body.uid;
-const product  = await Products.findById(pid)
-product.sold = false;
-product.buyer = "";
-await product.save()
+        const uid=req.params.uid;
+        const id=req.params.id;  //url parameters id
+        const userData=await Users.findById(uid)
+        const product=await Products.findById(id)
+        const index = userData.ads.indexOf(id);
+        if (index > -1) { // only splice array when item is found
+            userData.ads.splice(index, 1); // 2nd parameter means remove one item only
+        }
+        await userData.save()
+        // console.log(userData.ads)
+        res.status(200).json({ad:product,user:userData});
+
 
 res.status(200)
     } catch (error) {
