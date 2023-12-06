0912fb51fe55d35b111404f8278b3d178cb0552f
AvatarAarav <aarav.n21@iiits.in>
2023-12-06 12:40:33 +0530

    completing buy

--- CampusTradeClient/src/apis/index.js
+++ CampusTradeClient/src/apis/index.js
@@ -16,6 +16,6 @@ export const productADDAPI = (adData) => API.post("/form", adData);
 export const wishlistAPI = (uid,id) => API.get(`/ad/buy/${id}/uid/${uid}`) 
 export const googleLoginAPI = (googleObject) =>
   API.post("/user/google", googleObject);
-export const boughtAdAPI = (data) => API.post("/ad/bought",data)
+export const boughtAdAPI = (id,uid) => API.post("/ad/bought",{id,uid})
 
 
