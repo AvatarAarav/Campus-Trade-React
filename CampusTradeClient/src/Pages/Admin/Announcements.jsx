3dd0c9fd7e71cdbdbffdd30a96bf4f733c838602
KumarDevada <115659253+KumarDevada@users.noreply.github.com>
2024-02-23 14:41:18 +0530

    added college list board.

--- CampusTradeClient/src/Pages/Admin/Announcements.jsx
+++ CampusTradeClient/src/Pages/Admin/Announcements.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
-import { Grid , Paper, TextField, Button,  Typography, List, ListItem, ListItemText, Divider, ListItemAvatar, Avatar} from '@mui/material';
+import { Grid , Paper, TextField, Button,  Typography, List, ListItem, ListItemText, Divider, ListItemAvatar, Avatar, Box, Chip, Modal} from '@mui/material';
 
-import { Announcement, Mail, School } from '@mui/icons-material';
+import { Announcement, Mail, PlusOne, School } from '@mui/icons-material';
 import { sendAnnouncement } from '../../apis';
 
 const styles = {
@@ -90,64 +90,112 @@ const AnnouncementForm = () => {
   );
 };
 
+
+// Array of colleges
+const colleges = [
+  {
+    label: 'MIT',
+    cid: 0,
+  },
+  {
+    label: 'IIT Madras',
+    cid: 1,
+  },
+  {
+    label: 'NIT Rourkela',
+    cid: 2,
+  },
+  {
+    label: 'IIIT Sri City',
+    cid: 3,
+  },
+  {
+    label: 'IIT Kharagpur',
+    cid: 4,
+  },
+  {
+    label: 'NIT Suratkal',
+    cid: 5,
+  },
+  {
+    label: 'IIIT Hyderabad',
+    cid: 6,
+  },
+  {
+    label: 'IIT Patna',
+    cid: 7,
+  },
+  {
+    label: 'NIT Bhopal',
+    cid: 8,
+  },
+  {
+    label: 'IIIT Trichy',
+    cid: 9,
+  },
+  {
+    label: 'IIT Roorkee',
+    cid: 10,
+  },
+  {
+    label: 'NIT Trichy',
+    cid: 11,
+  },
+];
+
+
 const CollegeList = () => {
   // Dummy data for college list
-const colleges = [
-    {
-      label: 'MIT',
-      cid: 0,
-    },
-    {
-      label: 'IIT Madras',
-      cid: 1,
-    },
-    {
-      label: 'NIT Rourkela',
-      cid: 2,
-    },
-    {
-      label: 'IIIT Sri City',
-      cid: 3,
-    },
-    {
-      label: 'IIT Kharagpur',
-      cid: 4,
-    },
-    {
-      label: 'NIT Suratkal',
-      cid: 5,
-    },
-    {
-      label: 'IIIT Hyderabad',
-      cid: 6,
-    },
-    {
-      label: 'IIT Patna',
-      cid: 7,
-    },
-    {
-      label: 'NIT Bhopal',
-      cid: 8,
-    },
-    {
-      label: 'IIIT Trichy',
-      cid: 9,
-    },
-    {
-      label: 'IIT Roorkee',
-      cid: 10,
-    },
-    {
-      label: 'NIT Trichy',
-      cid: 11,
-    },
-  ];;
+
+  const [college, setcollege] = useState('');
+  const [open,setopen] = useState(false);
+
+  const handleopen = () => {
+    setopen(true);
+  }
+
+  const handleClose = () => {
+    setopen(false);
+  }
+
+  const addCollege = () => {
+    if(college === '') {
+      alert('Please fill the required feilds.')
+      return;
+    }
+    
+    colleges.push({label: college, cid: 999});
+    setopen(false);
+  }
+
+  
 
   return (
-    <Paper sx={{ padding: 2 }}>
+    <Paper sx={{ padding: 4 }}>
+      <Box display='flex' justifyContent='space-between' alignItems='center'>
           <Typography variant="h4" gutterBottom sx={{color:'grey'}}>
-            <School fontSize='large' color='warning'  /> List of Top Colleges 
+            <School fontSize='large' color='warning'  /> List of Top Colleges <sup style={{color:'green'}}>{colleges.length}</sup> 
           </Typography>
+
+          <Button variant='outlined' onClick={handleopen} color='secondary'>Add more +</Button>
+
+          <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+          >
+            <Box sx={{display:'flex', flexDirection:'column', justifyContent:'space-between',width:'580px', backgroundColor:'white', margin:'200px auto', padding:'30px', borderRadius:'10px'}}>
+              <Typography variant="h5" gutterBottom>Please fill the following fields...!</Typography>
+              <TextField id="collegename" label="College Name" fullWidth variant="outlined" value={college} onChange={(e) => setcollege(e.target.value)} />
+              <Box display='flex' justifyContent='flex-end' gap={2} marginTop={2}>
+                <Button variant="outlined" size="large" onClick={handleClose} color="success">cancel</Button>
+                <Button variant="contained" size="large" color="success" onClick={addCollege}>Confirm</Button>
+              </Box>
+            </Box>
+          </Modal>
+      </Box>
+          
           <List sx={{ width: '100%', height:'600px' ,overflow:'auto'}}>
             {colleges.map((a) => {
                 return (
