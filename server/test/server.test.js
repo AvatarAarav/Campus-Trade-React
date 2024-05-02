31362e4cfb24990a7f31859991e42f341544d713
bhparth <parthbhandari2663@gmail.com>
2024-05-02 16:53:44 +0530

    testing

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -1,20 +1,48 @@
+import fs from 'fs';
 import request from 'supertest';
 import app, { server } from '../server.js';
 
+// Create an instance of the app to reuse in tests
+const appInstance = request(app);
 
-
-describe('GET /', () => {
+describe('GET /api/user/allEmail', () => {
     it('should return 200 OK', async () => {
-        const response = await request(app).get('/api/user/allEmail');
+        const response = await appInstance.get('/api/user/allEmail');
         expect(response.status).toBe(200);
     });
 });
 
-// You can add more tests here
+describe('GET /api/admin_product', () => {
+    it('should return 200 OK', async () => {
+        const response = await appInstance.get('/api/admin_product');
+        expect(response.status).toBe(200);
+    });
+});
+
+describe('GET /api/search_admin_user', () => {
+    it('should return 200 OK', async () => {
+        const response = await appInstance.get('/api/search_admin_user');
+        expect(response.status).toBe(200);
+    });
+});
+
+describe('GET /api/user/:id', () => {
+    it('should return 200 OK', async () => {
+        const response = await appInstance.get('/api/user/65d785f419c16c09dcb43ea5');
+        expect(response.status).toBe(200);
+    });
+});
+
+describe('GET /api/search_result', () => {
+    it('should return 200 OK', async () => {
+        const response = await appInstance.get('/api/search_result');
+        expect(response.status).toBe(200);
+    });
+});
+
 // Close server after all tests
 afterAll(done => {
     server.close(() => {
-        // Forcefully exit after server is closed
-        done();
+    done();
     });
-});
+}, 10000); // Increase timeout to 10 seconds (adjust as needed)
